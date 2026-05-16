f32d6fb349a66e15e1d75b01723750bdef54445d
mohamedsherif14 <mohamed.sherif.nage.14@gmail.com>
2026-05-17 00:57:36 +0300

    user model

---
+++ models/User.model.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const bcrpyt = require('bcrypt');
+
+const userSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:[true , "name is required "]
+    },
+    email:{
+        type:String,
+        unique:true,
+        required:[true , "email is required "]
+    },
+    password:{
+        type:String,
+        required:[true , "password is required "]
+    },
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user",
+    }
+},{
+    timestamps:true
+});
+
+userSchema.pre('save' , async (next)=>{
+    if(this.isModified('password')){
+        this.password = await bcrypt.hash(this.password,12);
+    }
+})
+
+userSchema.methods.correctPassword = async  (inputPassword)=> {
+    return await bcrypt.compare(inputPassword, this.password);
+}
+
+module.exports = mongoose.model('User' , userSchema)
+
