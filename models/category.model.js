237d94ae2e5cc268ef619b30bb61bb3dc10db1d6
mohamedsherif14 <mohamed.sherif.nage.14@gmail.com>
2026-05-19 18:08:25 +0300

    category model

---
+++ models/category.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const categorySchema = mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    isActive:{
+        type:Boolean,
+        default:true
+    },
+    isDeleted:{
+        type:Boolean,
+        default:false,
+    }
+
+
+})
+
+module.exports = mongoose.model('category' , categorySchema);
